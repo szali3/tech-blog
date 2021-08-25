3add6b9a4afa0d255dc10bed86151e2d5e367de8
Zohaib Ali <s.zohaibali3@gmail.com>
2021-08-25 07:19:20 -0400

    fixed errors login and logout

--- public/javascript/logout.js
+++ public/javascript/logout.js
@@ -5,7 +5,7 @@ async function logout() {
     });
   
     if (response.ok) {
-      document.location.replace('/dashboard/');
+      document.location.replace('/');
     } else {
       alert(response.statusText);
     }
